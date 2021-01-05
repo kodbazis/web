969668482fea8faa86e37d87c83d6a65542eb160
Máté <bolgar.m@gmail.com>
2021-01-05 20:12:15 +0100

    nam collision fix

---
+++ public/app/13-cra/precache-manifest.759d839d4d1b46c99231e447f80b73f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bab31620b58396acac3d90e16d2519b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9880f8f04e55e348d8e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "20f695d9efe1157a34e5",
+    "url": "/static/css/main.165453e3.chunk.css"
+  },
+  {
+    "revision": "c9880f8f04e55e348d8e",
+    "url": "/static/js/2.8c3b2025.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c3b2025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f695d9efe1157a34e5",
+    "url": "/static/js/main.110e1b0b.chunk.js"
+  },
+  {
+    "revision": "033eaece93b68a0c85cd",
+    "url": "/static/js/runtime-main.af6a8661.js"
+  }
+]);
