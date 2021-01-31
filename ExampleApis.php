c56c4a2cc9746093272ac9687f72cdf1a2c46dc4
Máté <bolgar.m@gmail.com>
2021-01-31 04:42:17 +0100

    recipe app

--- ExampleApis.php
+++ ExampleApis.php
@@ -17,6 +17,7 @@ class ExampleApis
     public static function getRoutes(Pipeline $r, mysqli $conn, Environment $twig)
     {
         self::registerInstrumentRoutes($r);
+        self::registerRecipeRoutes($r);
         self::registerAuthRoutes($r);
     }
 
@@ -170,8 +171,6 @@ class ExampleApis
 
             setcookie('instrumentAPIID', $id, [
                 'expires' => time() + 60 * 60 * 24,
-                'secure' => true,
-                'samesite' => 'None',
             ]);
             $fileName = $id . '.json';
             copy($imagefileName,  $folder . '/' .  $fileName);
@@ -294,6 +293,166 @@ class ExampleApis
             echo json_encode(['id' => $request->vars['id']]);
         });
     }
+
+    public static function registerRecipeRoutes($r)
+    {
+        header("Access-Control-Allow-Origin:" . ($_SERVER['HTTP_ORIGIN'] ?? '*'));
+        header("Access-Control-Allow-Credentials: true");
+        $folder = __DIR__ . "/example-repos/recipes";
+        $imagefileName = __DIR__ . '/example-repos/recipes.json';
+
+        $getFileName = function ($folder) use ($imagefileName) {
+            if (isset($_COOKIE['recipesAPIID']) && @file_get_contents($folder . '/' . $_COOKIE['recipesAPIID'] . '.json')) {
+                return $_COOKIE['recipesAPIID'] . '.json';
+            }
+
+            $id = uniqid();
+
+            setcookie('recipesAPIID', $id, [
+                'expires' => time() + 60 * 60 * 24,
+            ]);
+            $fileName = $id . '.json';
+            copy($imagefileName,  $folder . '/' .  $fileName);
+            return $fileName;
+        };
+
+        $getContent = function ($folder) use ($imagefileName) {
+            $content = '';
+            if (isset($_COOKIE['recipesAPIID']) && @file_get_contents($folder . '/' . $_COOKIE['recipesAPIID'] . '.json')) {
+                $fileName = $_COOKIE['recipesAPIID'] . '.json';
+                $content = file_get_contents($folder . '/' .  $fileName);
+            } else {
+                $content = file_get_contents($imagefileName);
+            }
+            return $content;
+        };
+
+        $r->get('/recipe-app/api/recipes', function (Request $request) use ($folder, $getContent) {
+            header('Content-Type: application/json');
+            echo $getContent($folder);
+        });
+
+        $r->get('/recipe-app/static/images/{filename}', function (Request $request) {
+            $fileName = __DIR__ . '/example-repos/recipe-images/' . $request->vars['filename'];
+            $size = filesize($fileName);
+            header('Content-Type: image/jpeg');
+            header("Content-Length: $size bytes");
+            readfile($fileName);
+        });
+        $r->get('/recipe-app/static/assets/{filename}', function (Request $request) {
+            $fileName = __DIR__ . '/example-repos/recipe-images/' . $request->vars['filename'];
+            $size = filesize($fileName);
+            header('Content-Type: image/jpeg');
+            header("Content-Length: $size bytes");
+            readfile($fileName);
+        });
+
+        $r->post('/recipe-app/api/prune-recipes', function (Request $request) use ($folder) {
+            header('Content-Type: application/json');
+            if (($request->query['key'] ?? 0) !== ($_SERVER['MASTER_PW'] ?? 1)) {
+                http_response_code(401);
+                echo json_encode(['error' => 'unauthorized']);
+                return;
+            }
+            array_map('unlink', glob("$folder/*.*"));
+            rmdir($folder);
+            echo json_encode(['success' => true]);
+        });
+
+        $r->get('/recipe-app/api/recipes/{slug}', function (Request $request) use ($folder, $getContent) {
+            header('Content-Type: application/json');
+
+            $recipes = json_decode($getContent($folder), true);
+            $byId = array_values(array_filter($recipes, fn ($recipe) => $recipe['slug'] === $request->vars['slug']));
+            if (isset($byId[0])) {
+                return json_encode($byId[0]);
+            } else {
+                http_response_code(404);
+                return json_encode(['error' => 'not found']);
+            }
+        });
+
+        $r->post('/recipe-app/api/recipes', function (Request $request) use ($folder, $getFileName) {
+            header('Content-Type: application/json');
+
+            createPath($folder);
+            $fileName = $getFileName($folder);
+
+            $content = file_get_contents($folder . '/' .  $fileName);
+            $recipes = json_decode($content, true);
+            if (count($recipes) > 100) {
+                http_response_code(400);
+                echo ['error' => 'max size exceeded'];
+                return;
+            }
+
+            $newItem = [
+                "id" => uniqid(),
+                "name" => $request->body['name'],
+                "slug" => (new \Kodbazis\Generated\Slugifier\Slugifier())->slugify($request->body['name']),
+                "ingredients" => json_decode($request->body['ingredients']),
+                "steps" => json_decode($request->body['steps']),
+                "imageURL" => "placeholder.jpg"
+            ];
+            $recipes[] = $newItem;
+            file_put_contents($folder . '/' .  $fileName, json_encode($recipes, JSON_UNESCAPED_UNICODE));
+            echo json_encode($newItem);
+        });
+
+        $r->put('/recipe-app/api/recipes/{id}', function (Request $request) use ($folder, $getFileName) {
+            header('Content-Type: application/json');
+
+            createPath($folder);
+            $fileName = $getFileName($folder);
+
+            $content = file_get_contents($folder . '/' .  $fileName);
+            $recipes = json_decode($content, true);
+
+            $index = findIndex($recipes, fn ($instrument) => $instrument['id'] === $request->vars['id']);
+
+            if ($index === -1) {
+                http_response_code(404);
+                return json_encode(['error' => 'not found']);
+            }
+            $data  = file_get_contents('php://input');
+            $headers = [];
+            $request->body = parseFormData($data, $headers);
+
+            $newItem = [
+                'id' => $recipes[$index]['id'],
+                'name' => filter_var((string)$request->body['name'], FILTER_SANITIZE_STRING),
+                "slug" => $recipes[$index]['slug'],
+                "ingredients" => json_decode($request->body['ingredients']),
+                "steps" => json_decode($request->body['steps']),
+                "imageURL" => $recipes[$index]['imageURL'],
+            ];
+
+            $recipes[$index] = $newItem;
+            file_put_contents($folder . '/' .  $fileName, json_encode($recipes));
+            echo json_encode($newItem);
+        });
+
+        $r->delete('/recipe-app/api/recipes/{id}', function (Request $request) use ($folder, $getFileName) {
+            header('Content-Type: application/json');
+
+            createPath($folder);
+            $fileName = $getFileName($folder);
+
+            $content = file_get_contents($folder . '/' .  $fileName);
+            $recipes = json_decode($content, true);
+
+            $index = findIndex($recipes, fn ($recipe) => $recipe['id'] === $request->vars['id']);
+
+            if ($index === -1) {
+                http_response_code(404);
+                return json_encode(['error' => 'not found']);
+            }
+
+            array_splice($recipes, $index, 1);
+            file_put_contents($folder . '/' .  $fileName, json_encode($recipes));
+            echo json_encode(['id' => $request->vars['id']]);
+        });
+    }
 }
 
 
@@ -316,4 +475,97 @@ function createPath($path)
     $prevPath = substr($path, 0, strrpos($path, '/', -2) + 1);
     $return = createPath($prevPath);
     return ($return && is_writable($prevPath)) ? mkdir($path) : false;
+}
+
+function parseFormData($formData, &$header)
+{
+    $endOfFirstLine = strpos($formData, "\r\n");
+    $boundary = substr($formData, 0, $endOfFirstLine);
+    // Split form-data into each entry
+    $parts = explode($boundary, $formData);
+    $return = [];
+    $header = [];
+    // Remove first and last (null) entries
+    array_shift($parts);
+    array_pop($parts);
+    foreach ($parts as $part) {
+        $endOfHead = strpos($part, "\r\n\r\n");
+        $startOfBody = $endOfHead + 4;
+        $head = substr($part, 2, $endOfHead - 2);
+        $body = substr($part, $startOfBody, -2);
+        $headerParts = preg_split('#; |\r\n#', $head);
+        $key = null;
+        $thisHeader = [];
+        // Parse the mini headers,
+        // obtain the key
+        foreach ($headerParts as $headerPart) {
+            if (preg_match('#(.*)(=|: )(.*)#', $headerPart, $keyVal)) {
+                if ($keyVal[1] == "name") $key = substr($keyVal[3], 1, -1);
+                else {
+                    if ($keyVal[2] == "=") {
+                        $thisHeader[$keyVal[1]] = substr($keyVal[3], 1, -1);
+                    } else {
+                        $thisHeader[$keyVal[1]] = $keyVal[3];
+                    }
+                }
+            }
+        }
+        // If the key is multidimensional,
+        // generate multidimentional array
+        // based off of the parts
+        $nameParts = preg_split('#(?=\[.*\])#', $key);
+        if (count($nameParts) > 1) {
+            $current = &$return;
+            $currentHeader = &$header;
+            $l = count($nameParts);
+            for ($i = 0; $i < $l; $i++) {
+                // Strip array access tokens
+                $namePart = preg_replace('#[\[\]]#', "", $nameParts[$i]);
+
+                // If we are at the end of the depth of this entry,
+                // add data to array
+                if ($i == $l - 1) {
+                    if (isset($thisHeader['filename'])) {
+                        $filename = tempnam(sys_get_temp_dir(), "php");
+                        file_put_contents($filename, $body);
+                        $current[$namePart] = [
+                            "name" => $thisHeader['filename'],
+                            "type" => $thisHeader['Content-Type'],
+                            "tmp_name" => $filename,
+                            "error" => 0,
+                            "size" => count($body)
+                        ];
+                    } else {
+                        $current[$namePart] = $body;
+                    }
+                    $currentHeader[$namePart] = $thisHeader;
+                } else {
+                    // Advance into the array
+                    if (!isset($current[$namePart])) {
+                        $current[$namePart] = [];
+                        $currentHeader[$namePart] = [];
+                    }
+                    $current = &$current[$namePart];
+                    $currentHeader = &$currentHeader[$namePart];
+                }
+            }
+        } else {
+            if (isset($thisHeader['filename'])) {
+                $filename = tempnam(sys_get_temp_dir(), "php");
+                file_put_contents($filename, $body);
+                $return[$key] = [
+                    "name" => $thisHeader['filename'],
+                    "type" => $thisHeader['Content-Type'],
+                    "tmp_name" => $filename,
+                    "error" => 0,
+                    "size" => count($body)
+                ];
+            } else {
+                $return[$key] = $body;
+            }
+            $return[$key] = $body;
+            $header[$key] = $thisHeader;
+        }
+    }
+    return $return;
 }
---
+++ public/app/16-recipes/precache-manifest.df849f6a047626c2cf6462a305e3dff2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74a90ab081093b1408cf86549a697319",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a964e56b93a7fd9f1502",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7568374d533ff2aed239",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a964e56b93a7fd9f1502",
+    "url": "/static/js/2.a205146c.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/static/js/2.a205146c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7568374d533ff2aed239",
+    "url": "/static/js/main.927022d5.chunk.js"
+  },
+  {
+    "revision": "0f88517a9e86f7c1b087",
+    "url": "/static/js/runtime-main.ac996359.js"
+  }
+]);
