da08c39911c05693b8e7d01f4ded369a97e3206f
Máté <bolgar.m@gmail.com>
2021-02-08 14:29:27 +0100

    fix

--- PublicSite.php
+++ PublicSite.php
@@ -919,7 +919,7 @@ function renderEpisodeList($twig, $course, $allEpisodesInCourse)
     return $twig->render('intro-course-list.twig', [
         'isPromo' => true,
         'course' => $course,
-        'episodes' => alignToRows($allEpisodesInCourse, 4),
+        'episodes' => alignToRows($allEpisodesInCourse, 2),
     ]);
 }
 
